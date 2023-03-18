5eca7fceb3d004ac028a41c53042899ee62f9925
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2023-03-18 23:59:43 +0100

    added round page

---
+++ routes/round.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const pool = require('../db');
+const authorization = require('../middleware/authorization');
+router.get("/", authorization, async (req, res) => {
+    try {
+        const user = await pool.query("SELECT * FROM customers WHERE id = $1", [req.user.id]);
+        console.log(req.user.id);
+        res.json(user.rows[0]); 
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json("Server Error");
+    }
+});
+
+
+module.exports = router;
