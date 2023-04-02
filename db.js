07c4aa4d7213cfc37bf6405bc083c36d5cceaeb2
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2023-04-02 04:44:44 +0000

    changed db to cloud db

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'simo2013',
-    host: 'localhost',
+    password: 'eXHu4irRScCwmwG8MfDxGEQgZ3dNq5qS',
+    host: 'dpg-cgkg95u4dad69r2c1eg0-a',
     port: 5432,
     database: 'dartdb'
 });
