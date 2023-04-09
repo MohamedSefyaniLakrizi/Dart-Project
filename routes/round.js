461b7edeaf285edf6491b86847cc2fbe92ad8ff7
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2023-04-09 20:47:55 +0000

    added an await on the getUniqueInvitationCode

--- routes/round.js
+++ routes/round.js
@@ -39,7 +39,7 @@ router.post("/add", authorization, async (req, res) => {
 
     console.log("adding new Round");
     try {
-      const invitationCode = getUniqueInvitationCode();
+      const invitationCode = await getUniqueInvitationCode();
       console.log(invitationCode);
       const newRound = await pool.query(
         "INSERT INTO rounds (name, admin_id, amount, duration, invitation_code) VALUES ($1, $2, $3, $4, $5) RETURNING *",
