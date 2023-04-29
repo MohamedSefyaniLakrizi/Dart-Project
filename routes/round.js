5910dbc5b3b43b87e49db191de9dbb48756a033f
MohamedSefyaniLakrizi <mohamedsefyani@gmail.com>
2023-04-29 15:06:29 +0100

    tt

--- routes/round.js
+++ routes/round.js
@@ -121,8 +121,8 @@ module.exports = router;
   router.post('/add-by-invitation-code', authorization, async (req, res) => {
     try{
       const { invitationCode,  } = req.body;
-      const round = await pool.query("SELECT * FROM rounds WHERE invitation_code = $1;", [invitationCode]);
-      await pool.query("INSERT INTO participants (user_id, round_id) VALUES ($1, $2) RETURNING *;", [req.user.id, round.rows[0].id]);
+      const round = await pool.query("SELECT * FROM rounds WHERE invitation_code = $1", [invitationCode]);
+      await pool.query("INSERT INTO participants (user_id, round_id) VALUES ($1, $2) RETURNING *", [req.user.id, round.rows[0].id]);
       res.json("Round added successfully");
     } catch (error) {
       console.log(error.message);
@@ -148,10 +148,15 @@ module.exports = router;
 
   router.get("/get-round", async (req, res) => {
     try {
-      const round = await pool.query("SELECT * FROM participants WHERE user_id = $1;", [req.user.id]);
+      const round = await pool.query("SELECT * FROM participants WHERE user_id = $1", [req.user.id]);
+      if (round) {
+        res.json(round);
+      } else {
+        res.json(null);
+      }
       res.json(round.rows[0]);
     } catch (error) {
       console.log(error.message);
       res.status(500).json("Server Error");
     }
-  }); 
+  });   
